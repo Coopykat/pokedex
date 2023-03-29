5b3bcedf9af7adb64a12d3da8d1fedaf469a4b5a
Coopykat <kathycamazon@gmail.com>
2023-03-29 15:34:36 +0200

    quest number 4

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,27 +1,18 @@
-function PokemonCard() {
+function PokemonCard({pokemon}) {
 
-  const pokemon = pokemonList[0]    
    return (
    <figure className="card_img"> 
    {pokemon.imgSrc ? <img src={pokemon.imgSrc} alt={pokemon.name}/> : <p>"???"</p>}  
     <figcaption>{pokemon.name}</figcaption>   
      </figure>
      )   
+
  }
 
 
 // AJOUT NOUVELLE FONCTION QUETE TROIS //
 
-const pokemonList = [
-  {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
+
 
 
 
