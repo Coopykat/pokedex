c3f5cc7aab8ef11826a87d4862c8edef8e79fbf0
Coopykat <kathycamazon@gmail.com>
2023-03-31 11:46:13 +0200

    state pokemoncard added

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import PokemonCard from './components/PokemonCard'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import propTypes from 'prop-types'
 
 
 function App() {
@@ -16,19 +15,67 @@ const pokemonList = [
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
   },
   {
+    name: "charmander",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+  },
+  {
+    name: "squirtle",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  },
+  {
+    name: "pikachu",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+  },
+  {
     name: "mew",
   },
 ];
 
-const pokemon = pokemonList[0];
 
-  return (
-    <div>
-      <PokemonCard pokemon={pokemon}/>
-    </div>
 
-  );
+  // return (
+  //   <div>
+  //     <PokemonCard pokemon={pokemon}/>
+  //   </div>
+
+  // );
+
+
+  // STATE DES ETATS D'AME ERIC //
+
+
+
+const [pokemonIndex, setPokemonIndex] = useState(0);
+
+const handleClick = () => {
+  setPokemonIndex(pokemonIndex +1);
+}
+
+const handleLess = () => {
+  setPokemonIndex(pokemonIndex -1);
+}
+
+const pokemon = pokemonList[pokemonIndex];
+
+return (
+  <>
+  <PokemonCard pokemon={pokemon} />
+  {pokemonIndex > 0 &&<button onClick={handleLess}>Précédent</button>}
+  {pokemonIndex < pokemonList.length -1 &&<button onClick={handleClick}>Suivant</button>}
+  </>
+);
 };
+
+
+
+
+
+
+
+
 
 
 
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -18,7 +18,6 @@ PokemonCard.propTypes = {
 })
 
 }
-console.log(PokemonCard);
 
 
 
